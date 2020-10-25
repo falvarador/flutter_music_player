fa060209abf12d845f60107840b42814e27e31c5
falvarado <alvaguez1990@gmail.com>
2020-10-24 22:15:32 -0600

    Se crea el diseño del nombre de la canción y los botones de play y pause, además de su correspondientes animación. También el diseño de las liricas de la canción

--- lib/src/modules/player/music_player_page.dart
+++ lib/src/modules/player/music_player_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:music_player/src/modules/player/widgets/disk_progress.dart';
+
 import 'package:music_player/src/widgets/custom_appbar.dart';
+import 'package:music_player/src/modules/player/widgets/disk_progress.dart';
+import 'package:music_player/src/modules/player/widgets/lyrics.dart';
+import 'package:music_player/src/modules/player/widgets/song_name_player.dart';
 
 class MusicPlayerPage extends StatelessWidget {
   const MusicPlayerPage({Key key}) : super(key: key);
@@ -9,15 +12,47 @@ class MusicPlayerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: <Widget>[
-            CustomAppBar(),
-            Container(
-              child: DiskAndProgress(),
-            )
+            Background(),
+            Column(
+              children: <Widget>[
+                CustomAppBar(),
+                DiskAndProgress(),
+                SongNameAndButtomPlayer(),
+                Expanded(
+                  child: Lyrics(),
+                )
+              ],
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.70,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
+          gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.center,
+              colors: [
+                Color(0xff33333E),
+                Color(0xff201E28),
+              ])),
     );
   }
 }
